49fcfba6671e3edb4ae062fbc9802692137a34a2
SATYAMKAVAR <121211422+SATYAMKAVAR@users.noreply.github.com>
2023-12-08 21:46:12 +0530

    complete contactUs and rules component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RulesComponent } from './rules/rules.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-up',component:SignUpComponent},
+  {path:'sign-in',component:SignInComponent},
+  {path:'contact-us',component:ContactUsComponent},
+  {path:'rules',component:RulesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RulesComponent } from './rules/rules.component';
+import { LucideAngularModule, Mail , Contact , MapPinned} from 'lucide-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignUpComponent,
+    SignInComponent,
+    ContactUsComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    LucideAngularModule.pick({ Mail,Contact,MapPinned })
   ],
   providers: [],
   bootstrap: [AppComponent]
